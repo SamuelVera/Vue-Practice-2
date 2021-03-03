5464b7b3ff6671cd8e15e46120a2968739dde3d9
Samuel Vera <samuelvera1999@gmail.com>
2021-03-03 18:46:41 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
+<!--Vue single file component -->
+
+<!--Template of the component (HTML Structure)-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
+<!--Template script, code for the component-->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
+/**Exporting this single file component */
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
+<!--Styles of the component-->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
