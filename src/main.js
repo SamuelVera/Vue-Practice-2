a027bcdb1a6d53c1bd03310f655a3be309614637
Samuel Vera <samuelvera1999@gmail.com>
2021-03-04 12:34:34 -0400

    Second commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+/**Method that instantiates and injects the app into the DOM */
 import { createApp } from "vue";
+/**Root component */
 import App from "./App.vue";
 
+/**Mount the root component */
 createApp(App).mount("#app");
